0472883953c05d9c7e2e18516a887418c08d45f2
M Shashanka <shashanka318@gmail.com>
2024-08-07 18:48:53 +0530

    add middleware

--- main.go
+++ main.go
@@ -8,10 +8,21 @@ import (
 const port = "8080"
 const filepathRoot = "."
 
+type apiConfig struct {
+	fileServerHits int
+}
+
 func main() {
+
+	apiCfg := apiConfig{
+		fileServerHits: 0,
+	}
+
 	mux := http.NewServeMux()
-	mux.Handle("/app/*", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.Handle("/app/*", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
 	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.HandleFunc("/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("/reset", apiCfg.handlerReset)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
@@ -20,4 +31,11 @@ func main() {
 
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(srv.ListenAndServe())
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileServerHits++
+		next.ServeHTTP(w, r)
+	})
 }
