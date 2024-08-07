8952e9fac481090830cfe095dfcae6b9841f889a
M Shashanka <shashanka318@gmail.com>
2024-08-07 18:31:55 +0530

    basic routes

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/m-shashanka/go-server
+
+go 1.22.4
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+const port = "8080"
+const filepathRoot = "."
+
+func main() {
+	mux := http.NewServeMux()
+	mux.Handle("/app/*", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.HandleFunc("/healthz", handlerReadiness)
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	log.Printf("Serving on port: %s\n", port)
+	log.Fatal(srv.ListenAndServe())
+}
